8b5dccf4e189c21d358ffd398a5d9cd59f247b4d
Jeff Thorne <jeff.thorne@gmail.com>
2016-09-29 10:29:34 -0400

    added malware event call for calopera 2

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 from models.manager import Manager
 from datetime import datetime
 
-dsm = Manager("admin", "Trendmicro2008!")
+dsm = Manager("admin", "password")
 rangeFrom = datetime(2016, 9, 23, 11, 00)
 rangeTo = datetime(2016, 9, 23, 11, 44)
 dsm.antimalware_event_retreive(rangeFrom, rangeTo)
