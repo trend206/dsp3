2dff4a18fa3154781440b47a07e7492073f06716
Jeff Thorne <jeff.thorne@gmail.com>
2016-11-07 13:33:03 -0500

    starting on documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ import os
 # import sys
 
 sys.path.insert(0, os.path.abspath('../../..'))
-sys.path.insert(0, os.path.abspath('../../../dsp3'))
 
 
 # -- General configuration ------------------------------------------------
